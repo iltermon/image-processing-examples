cdb22eb7c5f433c501a61d048bebae3e8d3e2c54
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-04-09 20:01:03 +0300

    Create toGray.py

---
+++ toGray.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+def toGray(img):
+    height, width, channels = img.shape
+    n_img=np.zeros((height,width),np.uint8)
+
+    for i in range(height):
+        for j in range(width):
+            n_img[i,j]=img[i,j,0]*0.299 + img[i,j,1]*0.587 + img[i,j,2]*0.114
+    return n_img
+img = cv2.imread("E://resim.png")
+cv2.imshow("img",img)
+cv2.imshow("n_img",toGray(img))
+cv2.waitKey(0)
