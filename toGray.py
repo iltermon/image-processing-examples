c170e9959622b20b9745f8bf0981223d20a37c48
ilterkaankaraca <ilterkaankaraca@gmail.com>
2020-05-07 04:27:57 +0300

    Update toGray.py

--- toGray.py
+++ toGray.py
@@ -8,7 +8,3 @@ def toGray(img):
         for j in range(width):
             n_img[i,j]=img[i,j,0]*0.299 + img[i,j,1]*0.587 + img[i,j,2]*0.114
     return n_img
-img = cv2.imread("photos/resim.png")
-cv2.imshow("img",img)
-cv2.imshow("n_img",toGray(img))
-cv2.waitKey(0)
